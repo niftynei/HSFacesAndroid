01af884934070af3a667b114904ee12e28614820
macNugs <niftynei@gmail.com>
2013-10-01 23:59:44 -0400

    only log GA when not in debug mode

--- HackerSchool/src/main/java/knaps/hacker/school/BaseFragmentActivity.java
+++ HackerSchool/src/main/java/knaps/hacker/school/BaseFragmentActivity.java
@@ -12,12 +12,12 @@ public class BaseFragmentActivity extends FragmentActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        EasyTracker.getInstance(this).activityStart(this);
+        if (!BuildConfig.DEBUG) EasyTracker.getInstance(this).activityStart(this);
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        EasyTracker.getInstance(this).activityStop(this);
+        if (!BuildConfig.DEBUG) EasyTracker.getInstance(this).activityStop(this);
     }
 }
